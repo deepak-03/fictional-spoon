a5b6247227031c89740c5391eca72648117290a1
deepak-03 <42989439+deepak-03@users.noreply.github.com>
2020-07-04 04:12:46 +0530

    Update settings.py

--- level4_pro/learning_templates/learning_templates/settings.py
+++ level4_pro/learning_templates/learning_templates/settings.py
@@ -114,6 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS =  ["deepak03.pythonanywhere.com"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
